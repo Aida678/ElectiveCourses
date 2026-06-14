ddaae1e3fc5340a6a9521e8ab1c4e7ed3b6816ba
Aida678 <aidaisaeva989@gmail.com>
2026-06-15 00:16:47 +0300

    реализовала DatabaseManager - методы connect/disconnect

---
+++ database/DatabaseManager.cpp
@@ -0,0 +1,35 @@
+#include "DatabaseManager.h"
+
+DatabaseManager& DatabaseManager::instance()
+{
+    static DatabaseManager manager;
+    return manager;
+}
+
+bool DatabaseManager::connect(const QString& host, const QString& dbName,
+                              const QString& user, const QString& password,
+                              int port)
+{
+    m_db = QSqlDatabase::addDatabase("QPSQL");
+    m_db.setHostName(host);
+    m_db.setDatabaseName(dbName);
+    m_db.setUserName(user);
+    m_db.setPassword(password);
+    m_db.setPort(port);
+
+    if (!m_db.open()) {
+        qCritical() << "Ошибка подключения:" << m_db.lastError().text();
+        return false;
+    }
+
+    qDebug() << "Подключение к БД успешно!";
+    return true;
+}
+
+void DatabaseManager::disconnect()
+{
+    if (m_db.isOpen()) {
+        m_db.close();
+        qDebug() << "Соединение закрыто.";
+    }
+}
