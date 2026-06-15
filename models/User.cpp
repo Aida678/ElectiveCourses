7715e381dc3004ec21acb0dea7b38b4aaf5582fe
Aida678 <aidaisaeva989@gmail.com>
2026-06-15 23:41:09 +0300

    реализовала User - метод fromRecord

---
+++ models/User.cpp
@@ -0,0 +1,15 @@
+#include "User.h"
+
+User::User(int id, const QString& email, const QString& passwordHash)
+    : m_id(id), m_email(email), m_passwordHash(passwordHash)
+{
+}
+
+User User::fromRecord(const QSqlRecord& record)
+{
+    User user;
+    user.setId(record.value("id").toInt());
+    user.setEmail(record.value("email").toString());
+    user.setPasswordHash(record.value("password_hash").toString());
+    return user;
+}
