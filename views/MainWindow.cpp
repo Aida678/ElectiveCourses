f72f238c188b6bf2da834ba213bec58d903e59df
Aida678 <aidaisaeva989@gmail.com>
2026-06-18 16:23:47 +0300

    добавила MainWindow.cpp и mainwindow.ui - главное окно с вкладками

---
+++ views/MainWindow.cpp
@@ -0,0 +1,406 @@
+#include "MainWindow.h"
+#include "ui_mainwindow.h"
+#include "database/DatabaseManager.h"
+#include "controllers/StudentController.h"
+#include "controllers/TeacherController.h"
+#include "controllers/ElectiveController.h"
+#include "controllers/LearningController.h"
+#include <QMessageBox>
+#include <QTableWidgetItem>
+
+MainWindow::MainWindow(QWidget *parent, const User& user)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+    , m_currentUser(user)
+{
+    ui->setupUi(this);
+
+    setWindowTitle("Управление факультативами - " + m_currentUser.email());
+
+    // Подключаемся к БД, если не открыта
+    if (!DatabaseManager::instance().isOpen()) {
+        DatabaseManager::instance().connect("localhost", "ElectiveCoursesDB",
+                                            "postgres", "postgres");
+    }
+
+    // Создаём контроллеры
+    m_studentController = new StudentController(this, this);
+    m_teacherController = new TeacherController(this, this);
+    m_electiveController = new ElectiveController(this, this);
+    m_learningController = new LearningController(this, this);
+
+    // --- Вкладка "Студенты" ---
+    connect(ui->btnStudentAdd, &QPushButton::clicked, this, &MainWindow::onStudentAddClicked);
+    connect(ui->btnStudentUpdate, &QPushButton::clicked, this, &MainWindow::onStudentUpdateClicked);
+    connect(ui->btnStudentDelete, &QPushButton::clicked, this, &MainWindow::onStudentDeleteClicked);
+    connect(ui->btnStudentClear, &QPushButton::clicked, this, &MainWindow::onStudentClearClicked);
+    connect(ui->tableStudent, &QTableWidget::itemClicked, this, &MainWindow::onStudentTableClicked);
+
+    // --- Вкладка "Преподаватели" ---
+    connect(ui->btnTeacherAdd, &QPushButton::clicked, this, &MainWindow::onTeacherAddClicked);
+    connect(ui->btnTeacherUpdate, &QPushButton::clicked, this, &MainWindow::onTeacherUpdateClicked);
+    connect(ui->btnTeacherDelete, &QPushButton::clicked, this, &MainWindow::onTeacherDeleteClicked);
+    connect(ui->btnTeacherClear, &QPushButton::clicked, this, &MainWindow::onTeacherClearClicked);
+    connect(ui->tableTeacher, &QTableWidget::itemClicked, this, &MainWindow::onTeacherTableClicked);
+
+    // --- Вкладка "Факультативы" ---
+    connect(ui->btnElectiveAdd, &QPushButton::clicked, this, &MainWindow::onElectiveAddClicked);
+    connect(ui->btnElectiveUpdate, &QPushButton::clicked, this, &MainWindow::onElectiveUpdateClicked);
+    connect(ui->btnElectiveDelete, &QPushButton::clicked, this, &MainWindow::onElectiveDeleteClicked);
+    connect(ui->btnElectiveClear, &QPushButton::clicked, this, &MainWindow::onElectiveClearClicked);
+    connect(ui->tableElective, &QTableWidget::itemClicked, this, &MainWindow::onElectiveTableClicked);
+
+    // --- Вкладка "Обучение" ---
+    connect(ui->btnLearningAdd, &QPushButton::clicked, this, &MainWindow::onLearningAddClicked);
+    connect(ui->btnLearningUpdate, &QPushButton::clicked, this, &MainWindow::onLearningUpdateClicked);
+    connect(ui->btnLearningDelete, &QPushButton::clicked, this, &MainWindow::onLearningDeleteClicked);
+    connect(ui->btnLearningClear, &QPushButton::clicked, this, &MainWindow::onLearningClearClicked);
+    connect(ui->tableLearning, &QTableWidget::itemClicked, this, &MainWindow::onLearningTableClicked);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+    delete m_studentController;
+    delete m_teacherController;
+    delete m_electiveController;
+    delete m_learningController;
+}
+
+// === СТУДЕНТЫ ===
+void MainWindow::onStudentAddClicked()
+{
+    Student s;
+    s.setLastName(ui->lineStudentLastName->text().trimmed());
+    s.setFirstName(ui->lineStudentFirstName->text().trimmed());
+    s.setMiddleName(ui->lineStudentMiddleName->text().trimmed());
+    s.setPhone(ui->lineStudentPhone->text().trimmed());
+    s.setAddress(ui->lineStudentAddress->text().trimmed());
+    m_studentController->addStudent(s);
+}
+
+void MainWindow::onStudentUpdateClicked()
+{
+    Student s;
+    s.setId(ui->lineStudentId->text().toInt());
+    s.setLastName(ui->lineStudentLastName->text().trimmed());
+    s.setFirstName(ui->lineStudentFirstName->text().trimmed());
+    s.setMiddleName(ui->lineStudentMiddleName->text().trimmed());
+    s.setPhone(ui->lineStudentPhone->text().trimmed());
+    s.setAddress(ui->lineStudentAddress->text().trimmed());
+    m_studentController->updateStudent(s);
+}
+
+void MainWindow::onStudentDeleteClicked()
+{
+    int id = ui->lineStudentId->text().toInt();
+    m_studentController->deleteStudent(id);
+}
+
+void MainWindow::onStudentClearClicked()
+{
+    clearStudentInputFields();
+    showStatusMessage("Поля студентов очищены");
+}
+
+void MainWindow::onStudentTableClicked(int row, int column)
+{
+    Q_UNUSED(column);
+    m_studentController->selectStudent(row);
+}
+
+void MainWindow::updateStudentTable(const QList<Student>& students)
+{
+    ui->tableStudent->setRowCount(students.size());
+    ui->tableStudent->setColumnCount(6);
+    QStringList headers = {"ID", "Фамилия", "Имя", "Отчество", "Телефон", "Адрес"};
+    ui->tableStudent->setHorizontalHeaderLabels(headers);
+
+    for (int i = 0; i < students.size(); ++i) {
+        const Student& s = students[i];
+        ui->tableStudent->setItem(i, 0, new QTableWidgetItem(QString::number(s.id())));
+        ui->tableStudent->setItem(i, 1, new QTableWidgetItem(s.lastName()));
+        ui->tableStudent->setItem(i, 2, new QTableWidgetItem(s.firstName()));
+        ui->tableStudent->setItem(i, 3, new QTableWidgetItem(s.middleName()));
+        ui->tableStudent->setItem(i, 4, new QTableWidgetItem(s.phone()));
+        ui->tableStudent->setItem(i, 5, new QTableWidgetItem(s.address()));
+    }
+    ui->tableStudent->resizeColumnsToContents();
+    ui->tableStudent->horizontalHeader()->setStretchLastSection(true);
+}
+
+void MainWindow::clearStudentInputFields()
+{
+    ui->lineStudentId->clear();
+    ui->lineStudentLastName->clear();
+    ui->lineStudentFirstName->clear();
+    ui->lineStudentMiddleName->clear();
+    ui->lineStudentPhone->clear();
+    ui->lineStudentAddress->clear();
+    ui->btnStudentUpdate->setEnabled(false);
+    ui->btnStudentDelete->setEnabled(false);
+}
+
+void MainWindow::setStudentInputFields(const Student& s)
+{
+    ui->lineStudentId->setText(QString::number(s.id()));
+    ui->lineStudentLastName->setText(s.lastName());
+    ui->lineStudentFirstName->setText(s.firstName());
+    ui->lineStudentMiddleName->setText(s.middleName());
+    ui->lineStudentPhone->setText(s.phone());
+    ui->lineStudentAddress->setText(s.address());
+    ui->btnStudentUpdate->setEnabled(true);
+    ui->btnStudentDelete->setEnabled(true);
+}
+
+// === ПРЕПОДАВАТЕЛИ ===
+void MainWindow::onTeacherAddClicked()
+{
+    Teacher t;
+    t.setLastName(ui->lineTeacherLastName->text().trimmed());
+    t.setFirstName(ui->lineTeacherFirstName->text().trimmed());
+    t.setMiddleName(ui->lineTeacherMiddleName->text().trimmed());
+    t.setPhone(ui->lineTeacherPhone->text().trimmed());
+    t.setUserId(ui->lineTeacherUserId->text().toInt());
+    m_teacherController->addTeacher(t);
+}
+
+void MainWindow::onTeacherUpdateClicked()
+{
+    Teacher t;
+    t.setId(ui->lineTeacherId->text().toInt());
+    t.setLastName(ui->lineTeacherLastName->text().trimmed());
+    t.setFirstName(ui->lineTeacherFirstName->text().trimmed());
+    t.setMiddleName(ui->lineTeacherMiddleName->text().trimmed());
+    t.setPhone(ui->lineTeacherPhone->text().trimmed());
+    t.setUserId(ui->lineTeacherUserId->text().toInt());
+    m_teacherController->updateTeacher(t);
+}
+
+void MainWindow::onTeacherDeleteClicked()
+{
+    int id = ui->lineTeacherId->text().toInt();
+    m_teacherController->deleteTeacher(id);
+}
+
+void MainWindow::onTeacherClearClicked()
+{
+    clearTeacherInputFields();
+    showStatusMessage("Поля преподавателей очищены");
+}
+
+void MainWindow::onTeacherTableClicked(int row, int column)
+{
+    Q_UNUSED(column);
+    m_teacherController->selectTeacher(row);
+}
+
+void MainWindow::updateTeacherTable(const QList<Teacher>& teachers)
+{
+    ui->tableTeacher->setRowCount(teachers.size());
+    ui->tableTeacher->setColumnCount(6);
+    QStringList headers = {"ID", "Фамилия", "Имя", "Отчество", "Телефон", "User ID"};
+    ui->tableTeacher->setHorizontalHeaderLabels(headers);
+
+    for (int i = 0; i < teachers.size(); ++i) {
+        const Teacher& t = teachers[i];
+        ui->tableTeacher->setItem(i, 0, new QTableWidgetItem(QString::number(t.id())));
+        ui->tableTeacher->setItem(i, 1, new QTableWidgetItem(t.lastName()));
+        ui->tableTeacher->setItem(i, 2, new QTableWidgetItem(t.firstName()));
+        ui->tableTeacher->setItem(i, 3, new QTableWidgetItem(t.middleName()));
+        ui->tableTeacher->setItem(i, 4, new QTableWidgetItem(t.phone()));
+        ui->tableTeacher->setItem(i, 5, new QTableWidgetItem(QString::number(t.userId())));
+    }
+    ui->tableTeacher->resizeColumnsToContents();
+    ui->tableTeacher->horizontalHeader()->setStretchLastSection(true);
+}
+
+void MainWindow::clearTeacherInputFields()
+{
+    ui->lineTeacherId->clear();
+    ui->lineTeacherLastName->clear();
+    ui->lineTeacherFirstName->clear();
+    ui->lineTeacherMiddleName->clear();
+    ui->lineTeacherPhone->clear();
+    ui->lineTeacherUserId->clear();
+    ui->btnTeacherUpdate->setEnabled(false);
+    ui->btnTeacherDelete->setEnabled(false);
+}
+
+void MainWindow::setTeacherInputFields(const Teacher& t)
+{
+    ui->lineTeacherId->setText(QString::number(t.id()));
+    ui->lineTeacherLastName->setText(t.lastName());
+    ui->lineTeacherFirstName->setText(t.firstName());
+    ui->lineTeacherMiddleName->setText(t.middleName());
+    ui->lineTeacherPhone->setText(t.phone());
+    ui->lineTeacherUserId->setText(QString::number(t.userId()));
+    ui->btnTeacherUpdate->setEnabled(true);
+    ui->btnTeacherDelete->setEnabled(true);
+}
+
+// === ФАКУЛЬТАТИВЫ ===
+void MainWindow::onElectiveAddClicked()
+{
+    Elective e;
+    e.setName(ui->lineElectiveName->text().trimmed());
+    e.setDepartmentId(ui->lineElectiveDepartmentId->text().toInt());
+    m_electiveController->addElective(e);
+}
+
+void MainWindow::onElectiveUpdateClicked()
+{
+    Elective e;
+    e.setId(ui->lineElectiveId->text().toInt());
+    e.setName(ui->lineElectiveName->text().trimmed());
+    e.setDepartmentId(ui->lineElectiveDepartmentId->text().toInt());
+    m_electiveController->updateElective(e);
+}
+
+void MainWindow::onElectiveDeleteClicked()
+{
+    int id = ui->lineElectiveId->text().toInt();
+    m_electiveController->deleteElective(id);
+}
+
+void MainWindow::onElectiveClearClicked()
+{
+    clearElectiveInputFields();
+    showStatusMessage("Поля факультативов очищены");
+}
+
+void MainWindow::onElectiveTableClicked(int row, int column)
+{
+    Q_UNUSED(column);
+    m_electiveController->selectElective(row);
+}
+
+void MainWindow::updateElectiveTable(const QList<Elective>& electives)
+{
+    ui->tableElective->setRowCount(electives.size());
+    ui->tableElective->setColumnCount(3);
+    QStringList headers = {"ID", "Название", "Кафедра ID"};
+    ui->tableElective->setHorizontalHeaderLabels(headers);
+
+    for (int i = 0; i < electives.size(); ++i) {
+        const Elective& e = electives[i];
+        ui->tableElective->setItem(i, 0, new QTableWidgetItem(QString::number(e.id())));
+        ui->tableElective->setItem(i, 1, new QTableWidgetItem(e.name()));
+        ui->tableElective->setItem(i, 2, new QTableWidgetItem(QString::number(e.departmentId())));
+    }
+    ui->tableElective->resizeColumnsToContents();
+    ui->tableElective->horizontalHeader()->setStretchLastSection(true);
+}
+
+void MainWindow::clearElectiveInputFields()
+{
+    ui->lineElectiveId->clear();
+    ui->lineElectiveName->clear();
+    ui->lineElectiveDepartmentId->clear();
+    ui->btnElectiveUpdate->setEnabled(false);
+    ui->btnElectiveDelete->setEnabled(false);
+}
+
+void MainWindow::setElectiveInputFields(const Elective& e)
+{
+    ui->lineElectiveId->setText(QString::number(e.id()));
+    ui->lineElectiveName->setText(e.name());
+    ui->lineElectiveDepartmentId->setText(QString::number(e.departmentId()));
+    ui->btnElectiveUpdate->setEnabled(true);
+    ui->btnElectiveDelete->setEnabled(true);
+}
+
+// === ОБУЧЕНИЕ ===
+void MainWindow::onLearningAddClicked()
+{
+    StudentLearning l;
+    l.setGrade(ui->lineLearningGrade->text().toInt());
+    l.setExamDate(ui->dateLearningExam->date());
+    l.setTeacherId(ui->lineLearningTeacherId->text().toInt());
+    l.setSemesterCourseId(ui->lineLearningSemesterCourseId->text().toInt());
+    l.setStudentId(ui->lineLearningStudentId->text().toInt());
+    m_learningController->addLearning(l);
+}
+
+void MainWindow::onLearningUpdateClicked()
+{
+    StudentLearning l;
+    l.setId(ui->lineLearningId->text().toInt());
+    l.setGrade(ui->lineLearningGrade->text().toInt());
+    l.setExamDate(ui->dateLearningExam->date());
+    l.setTeacherId(ui->lineLearningTeacherId->text().toInt());
+    l.setSemesterCourseId(ui->lineLearningSemesterCourseId->text().toInt());
+    l.setStudentId(ui->lineLearningStudentId->text().toInt());
+    m_learningController->updateLearning(l);
+}
+
+void MainWindow::onLearningDeleteClicked()
+{
+    int id = ui->lineLearningId->text().toInt();
+    m_learningController->deleteLearning(id);
+}
+
+void MainWindow::onLearningClearClicked()
+{
+    clearLearningInputFields();
+    showStatusMessage("Поля обучения очищены");
+}
+
+void MainWindow::onLearningTableClicked(int row, int column)
+{
+    Q_UNUSED(column);
+    m_learningController->selectLearning(row);
+}
+
+void MainWindow::updateLearningTable(const QList<StudentLearning>& learnings)
+{
+    ui->tableLearning->setRowCount(learnings.size());
+    ui->tableLearning->setColumnCount(7);
+    QStringList headers = {"ID", "Оценка", "Дата", "Teacher ID", "SemCourse ID", "Student ID"};
+    ui->tableLearning->setHorizontalHeaderLabels(headers);
+
+    for (int i = 0; i < learnings.size(); ++i) {
+        const StudentLearning& l = learnings[i];
+        ui->tableLearning->setItem(i, 0, new QTableWidgetItem(QString::number(l.id())));
+        ui->tableLearning->setItem(i, 1, new QTableWidgetItem(QString::number(l.grade())));
+        ui->tableLearning->setItem(i, 2, new QTableWidgetItem(l.examDate().toString("dd.MM.yyyy")));
+        ui->tableLearning->setItem(i, 3, new QTableWidgetItem(QString::number(l.teacherId())));
+        ui->tableLearning->setItem(i, 4, new QTableWidgetItem(QString::number(l.semesterCourseId())));
+        ui->tableLearning->setItem(i, 5, new QTableWidgetItem(QString::number(l.studentId())));
+    }
+    ui->tableLearning->resizeColumnsToContents();
+    ui->tableLearning->horizontalHeader()->setStretchLastSection(true);
+}
+
+void MainWindow::clearLearningInputFields()
+{
+    ui->lineLearningId->clear();
+    ui->lineLearningGrade->clear();
+    ui->dateLearningExam->setDate(QDate::currentDate());
+    ui->lineLearningTeacherId->clear();
+    ui->lineLearningSemesterCourseId->clear();
+    ui->lineLearningStudentId->clear();
+    ui->btnLearningUpdate->setEnabled(false);
+    ui->btnLearningDelete->setEnabled(false);
+}
+
+void MainWindow::setLearningInputFields(const StudentLearning& l)
+{
+    ui->lineLearningId->setText(QString::number(l.id()));
+    ui->lineLearningGrade->setText(QString::number(l.grade()));
+    ui->dateLearningExam->setDate(l.examDate());
+    ui->lineLearningTeacherId->setText(QString::number(l.teacherId()));
+    ui->lineLearningSemesterCourseId->setText(QString::number(l.semesterCourseId()));
+    ui->lineLearningStudentId->setText(QString::number(l.studentId()));
+    ui->btnLearningUpdate->setEnabled(true);
+    ui->btnLearningDelete->setEnabled(true);
+}
+
+void MainWindow::showStatusMessage(const QString& msg, bool isError)
+{
+    if (isError)
+        ui->statusbar->setStyleSheet("color: red;");
+    else
+        ui->statusbar->setStyleSheet("");
+    ui->statusbar->showMessage(msg, 5000);
+}
